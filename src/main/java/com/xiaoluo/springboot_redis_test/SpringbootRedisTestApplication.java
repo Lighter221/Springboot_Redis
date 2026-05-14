f66394a23f36124e06dcd1de380b832a04f545e1
Lighter221 <3523493872@qq.com>
2026-05-14 11:22:35 +0800

    test

---
+++ src/main/java/com/xiaoluo/springboot_redis_test/SpringbootRedisTestApplication.java
@@ -0,0 +1,13 @@
+package com.xiaoluo.springboot_redis_test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootRedisTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootRedisTestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/xiaoluo/springboot_redis_test/SpringbootRedisTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xiaoluo.springboot_redis_test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootRedisTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
